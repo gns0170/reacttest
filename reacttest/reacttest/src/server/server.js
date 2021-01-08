1ebbe5769d9589d02b341ecf42b148b8095ae965
gns0170 <gns0170>
2021-01-09 08:48:09 +0900

    server2

--- reacttest/reacttest/src/server/server.js
+++ reacttest/reacttest/src/server/server.js
@@ -1,44 +1,101 @@
+if(process.env.NODE_ENV!=='production'){
+    require('dotenv').config()
+}
 
 
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override')
+
+const initializePassport = require('./passport-config');
+initializePassport(
+    passport, 
+    username => users.find(user=> user.username === username),
+    id => users.find(user => user.id === id)
+)
+
+const users = [];
 
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:false}))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized:false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 
 
-const cors = require('cors');
-app.use(cors({
-    method:['GET','POST'],
-    origin: 'http://localhost:3000',
-    credentials : true,
-}));
 
-
-app.get('/', (req,res,next)=>{
-    res.send("hi11");
-    next();
+app.get('/', checkAuthenticated, (req,res)=>{
+    res.render('home',{name:req.user.name})
 })
 
-/*
-const passport = require('passport');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-var LocalStrategy = require('passport-local').Strategy;
 
-app.use(cookieParser('keyboard cat'));
-app.use(session({secret: 'keyboard cat'}));
-app.use(passport.initialize());
-app.use(passport.session());
-*/
 
-app.use('/api',(req,res,next)=>
-    
-    res.json('hi')
+app.get('/login', checkNotAuthenticated,(req,res,next)=>{
+    res.render('login')
+})
 
-);
+
+app.get('/register', checkNotAuthenticated,(req,res,next)=>{
+    res.render('register')
+})
+
+app.post('/login',checkNotAuthenticated,passport.authenticate('local',{
+    successRedirect:'/',
+    failureRedirect: '/login',
+    failureFlash : true
+}))
+
+app.post('/register',checkNotAuthenticated,async(req,res)=>{
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password,10)
+        users.push({
+            id: Date.now().toString(),
+            name : req.body.name,
+            username : req.body.username,
+            password : hashedPassword
+        })
+        res.redirect('login')
+    }catch{
+        res.redirect('/register')
+    }
+    console.log(users);
+})
+
+
+
+app.delete('/logout', (req,res)=>{
+    req.logOut()
+    res.redirect('/login')
+})
+
+
+function checkAuthenticated(req,res,next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req,res,next){
+    if(req.isAuthenticated()){
+        return res.redirect('/')
+    }
+    next()
+}
+
+
 
 
 
@@ -46,6 +103,3 @@ app.use('/api',(req,res,next)=>
 app.listen(4000,()=>{
     console.log('Success')
 })
-
-
-
